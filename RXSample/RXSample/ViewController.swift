208b2448d448e7f70fc4acb284f970fb6e930f9a
Varun Kumar <varun>
2019-11-21 15:49:03 +0530

    RX project start

---
+++ RXSample/RXSample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RXSample
+//
+//  Created by Varun Kumar on 21/11/19.
+//  Copyright © 2019 VK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
